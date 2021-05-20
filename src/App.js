6da40fc7498df387adaadff0a17fd6bf67fc9fc1
Peeranut <6031305022@lamduan.mfu.ac.th>
2021-05-21 02:02:13 +0700

    show data from patientCard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import './App.css';
 import PatientCard from './components/patientCard';
 import DisplayCard from './components/displayCard';
 function App() {
   const [inputData, setInputData] = useState();
-  function getData(key,value) {
+  const [storePatientInfo, setStorePatientInfo] = useState();
+
+  function getData(key, value) {
     const data = inputData;
-    setInputData({ ...data,[key]: value});
-}
+    setInputData({ ...data, [key]: value });
+    if (key == 'patient') {
+      setStorePatientInfo(value);
+    }
+  }
+
+
   return (
     <Container>
       <Row>
-        <Col sm={5}>ข้อมูลผู้ป่วย 
-          <PatientCard returnData={getData}/>
+        <Col sm={5}>ข้อมูลผู้ป่วย
+          <PatientCard returnData={getData} />
           {/* <PatientCard /> input 2 Dynamic */}
         </Col>
         <Col sm={7}>Timeline
-          <DisplayCard /> output
+          <DisplayCard patientData={storePatientInfo} /> output
         </Col>
       </Row>
     </Container>
--- src/components/displayCard.js
+++ src/components/displayCard.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 
-class displayCard extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>ผู้ป่วย</h1>
-            </div>
-        );
-    }
+function DisplayCard({ patientData }) {
+
+    return (
+        <div>
+            <h1>ผู้ป่วย{patientData && patientData.gender}</h1>
+            <h1>อายุ{patientData && patientData.age}</h1>
+            <h1>อาชีพ{patientData && patientData.career}</h1>
+        </div>
+    );
 }
 
-export default displayCard;
+export default DisplayCard;
