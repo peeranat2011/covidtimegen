dad1b8d79f2ea1dc450270260f61d947e304f8f0
Peeranut <6031305022@lamduan.mfu.ac.th>
2021-05-26 02:10:20 +0700

    handle data call back, filter data display &stylin

--- src/components/displayCard.js
+++ src/components/displayCard.js
@@ -1,15 +1,78 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
+import '../App.css'
+function DisplayCard({ patientData, timeLineData, onDelete }) {
 
-function DisplayCard({ patientData }) {
+    function dateFormat(timeLineData) {
+        return timeLineData && timeLineData.slice(0, 10);
+    }
 
+    const uniqueSumDate = timeLineData && timeLineData
+        .map((item) => dateFormat(item.datetime))
+        .filter((v, i, s) => s.indexOf(v) === i);
+
+    let filtered = {}
+
+    for (let i = 0; i < uniqueSumDate.length; i++) {
+        const arr = [];
+        timeLineData.forEach((time, index) => {
+            if (dateFormat(time.datetime) == uniqueSumDate[i]) {
+                arr.push(time)
+                filtered[dateFormat(time.datetime)] = arr;
+            }
+        })
+    }
+    const handleDelete = (id) => {
+        onDelete(id);
+    };
+    return (
+        <div class="app_container">
+            <h1 class="text-center" style={{ color: "rgb(255, 193, 7)", padding: "4% 0px 1% 0px" }}>Timeline</h1>
+            <div class="dis_patient_con">
+                <h4 class="text-center">ผู้ป่วย{patientData[0] && [...patientData].pop().gender} อายุ {patientData[0] && [...patientData].pop().age} ปี</h4>
+                <p style={{fontSize: "14px", fontWeight: "bold" }} class="text-center">อาชีพ {patientData[0] && [...patientData].pop().career}</p>
+            </div>
+            {
+                Object.keys(filtered).map((date, i) => {
+                    return (
+                        <ShowTimeline key={i} header={date} items={filtered[date]} cbDelete={handleDelete} data={filtered} />
+                    );
+                })
+            }
+        </div >
+    );
+}
+
+function ShowTimeline({ header, items, cbDelete, data }) {
+
+
+    function onDelete(id) {
+        cbDelete(id);
+        const delLi = document.getElementById(id);
+        delLi.className = 'fade-out-right';
+        
+
+    }
+    const date = new Date(header).toLocaleDateString();
     return (
         <div>
-            <h1>ผู้ป่วย{patientData && patientData.gender}</h1>
-            <h1>อายุ{patientData && patientData.age}</h1>
-            <h1>อาชีพ{patientData && patientData.career}</h1>
+            <ul class="timeline">
+                <li class="event fade-in" data-date={date}>
+                    <li class="fade-in">
+                        {items &&
+                            items.map((item, index) => {
+                                return (
+                                    <li class="fade-in detailed_cointainer right" id={item.id} key={index} >
+                                        <div><label style={{color: "rgb(255, 193, 7)"}}>{item.datetime.slice(11, 16)}</label> {item.detail}</div>  <button onClick={(e) => onDelete(item.id)} style={{background:"transparent",border:"none",color:"white"}}>x</button>
+                                    </li>
+                                );
+                            })}
+                    </li>
+                </li>
+            </ul>
         </div>
     );
 }
+
 
 export default DisplayCard;
--- src/components/patientCard.js
+++ src/components/patientCard.js
@@ -1,38 +1,111 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-
-function PatientCard({returnData}) {
+import { Container, Row, Col } from 'react-bootstrap';
+let timeline = [];
+let patientArr = [];
+function PatientCard({ returnPatientData, returnTimeLineData }) {
 
     const [patientData, setPatientData] = useState({});
+    const [timeLineData, setTimeLineData] = useState([]);
 
     function handleOnChange({ target }) {
         const data = patientData;
-        setPatientData({ ...data,[target.name]: target.value });
-        returnData('patient',patientData);
+        setPatientData({ ...data, [target.name]: target.value });
+
     }
+
+    function addItem({ target }) {
+        let genId = new Date().getTime();
+        const timeData = timeLineData;
+        setTimeLineData({ ...timeData, id: genId, [target.name]: target.value });
+
+    }
+
+    function subMitData() {
+        patientArr.push(patientData)
+        timeline.push(timeLineData);
+        timeline.sort((a, b) => (a.datetime > b.datetime) ? 1 : ((b.datetime > a.datetime) ? -1 : 0))
+        returnPatientData('patient', patientArr);
+        returnTimeLineData('timeline', timeline);
+    }
+
+
     return (
+
         <div>
-            <select name="gender" onChange={handleOnChange}>
-                <option value="ชาย">ชาย</option>
-                <option value="หญิง">หญิง</option>
-            </select>
-            <label>
-                อายุ:
-                <input
-                    name="age"
-                    onChange={handleOnChange}
-                    type="number"
-                />
-            </label>
-            <label>
-                อาชีพ:
-                <input
-                    name="career"
-                    onChange={handleOnChange}
-                    type="text"
-                />
-            </label>
+            <div class="col-12">
+
+                <div class="row input_container">
+                    <div class="col-12" style={{ height: "3rem" }}>
+                        <label className="patient_header">ข้อมูลผู้ป่วย</label></div>
+
+                    <div class="col-sm-6 justify-content-center" >
+                        <label>เพศ</label>
+                        <select name="gender" onChange={handleOnChange} class="gender_option">
+                            <option value=""></option>
+                            <option value="ชาย">ชาย</option>
+                            <option value="หญิง">หญิง</option>
+                        </select></div>
+                    <div class="col-sm-6 justify-content-center">
+                        <label>
+                            อายุ</label>
+                        <input
+                            name="age"
+                            type="number"
+                            onChange={handleOnChange}
+                        />
+                    </div>
+                    <div class="col-12" style={{
+                        paddingTop: "3%",
+                        paddingBottom: "9%"
+                    }}>
+                        <label>อาชีพ</label>
+                        <input
+                            name="career"
+                            type="text"
+                            onChange={handleOnChange}
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12" style={{ paddingTop: "7%" }}>
+
+                <div class="row input_container">
+                    <div class="col-12" style={{ height: "3rem" }}>
+                        <label className="patient_header">ข้อมูลไทม์ไลน์</label></div>
+
+                    <div class="col-12">
+                        <label >
+                            วันเวลา
+                            </label>
+                        <input
+                            name="datetime"
+                            type="datetime-local"
+                            onChange={addItem}
+                            required="true"
+                        />
+
+                    </div>
+                    <div class="col-12" style={{ paddingTop: "4%" }}>
+                        <label>
+                            รายละเอียด
+                            </label>
+                        <input
+                            class="input_detailed"
+                            name="detail"
+                            onChange={addItem}
+                            required="true"
+                        />
+
+                    </div>
+                    <div class="col-12" style={{ paddingTop: "4%", paddingBottom: "4%" }}>
+                        <button class="adding_button" onClick={subMitData}>+ เพิ่มข้อมูล</button></div>
+
+                </div>
+            </div>
         </div>
+
     );
 
 }
